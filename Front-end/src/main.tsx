05a72241b2ff5fec237a70f1f7dfdedb64bfb837
jadelmir <31207632+jadelmir@users.noreply.github.com>
2026-08-06 11:22:37 -0400

    feat: make router respect Vite base URL

--- Front-end/src/main.tsx
+++ Front-end/src/main.tsx
@@ -1,12 +1,14 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import { BrowserRouter } from "react-router";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const routerBase = import.meta.env.BASE_URL === "/"
+  ? "/"
+  : import.meta.env.BASE_URL.replace(/\/$/, "");
 
-  createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
-  
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={routerBase}>
+    <App />
+  </BrowserRouter>,
+);
